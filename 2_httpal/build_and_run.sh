ccd76aeb00f9a8ebea37eabba5b72daf862b44e0
stun100 <baedaniele01@gmail.com>
2026-02-18 19:01:02 +0100

    remove wave equation project

---
+++ 2_httpal/build_and_run.sh
@@ -0,0 +1,1 @@
+gcc -o httpal httpal.c && ./httpal
--- 2_wave_equation/build_and_run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gcc main.c -o main.out -I/opt/homebrew/include -L/opt/homebrew/lib -lraylib -framework OpenGL -framework Cocoa -framework IOKit -framework CoreVideo && ./main.out
